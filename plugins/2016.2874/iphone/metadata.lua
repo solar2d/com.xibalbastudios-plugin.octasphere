33c6147d6e52256026b142030f1a19dfc0e4f5b2
ggcrunchy <steve@xibalbastudios.com>
2022-11-02 16:20:11 -0500

    First go at octasphere

---
+++ plugins/2016.2874/iphone/metadata.lua
@@ -0,0 +1,13 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'plugin_octasphere' },
+		frameworks = {},
+		frameworksOptional = {},
+	},
+}
+
+
+return metadata
